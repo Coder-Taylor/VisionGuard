ff73a5569e70704a64cf49b5919fa3b07439440b
Taylor <642132880@qq.com>
2026-05-06 14:06:34 +0800

    fix: cloud-deploy internal/config 同步 PublicBaseURL 字段

--- cloud-deploy/internal/config/config.go
+++ cloud-deploy/internal/config/config.go
@@ -23,6 +23,8 @@ type Config struct {
 
 	DoubaoAPIKey string
 	DoubaoAPIURL string
+
+	PublicBaseURL string // 服务器公网 URL，用于生成豆包可访问的图片地址
 }
 
 func Load() *Config {
@@ -47,6 +49,7 @@ func Load() *Config {
 
 		DoubaoAPIKey: getEnv("DOUBAO_API_KEY", ""),
 		DoubaoAPIURL: getEnv("DOUBAO_API_URL", "https://ark.cn-beijing.volces.com"),
+		PublicBaseURL: getEnv("PUBLIC_BASE_URL", "http://localhost:3000"),
 	}
 }
 
